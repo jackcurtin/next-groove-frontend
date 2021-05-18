15e5224ab13762b0e13a8cf718f3d4c51e4b84ba
Jack Curtin <jackcurtin94@gmail.com>
2021-05-18 14:41:55 -0500

    albums populate page

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AlbumService} from '../services/album/album.service';
 
 @Component({
   selector: 'app-browse',
@@ -6,11 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./browse.component.css']
 })
 export class BrowseComponent implements OnInit {
-  albums = ['fake', 'man'];
+  albums = [];
 
-  constructor() { }
+  constructor(private albumService: AlbumService) { }
+
+  getAlbums(): any {
+    this.albumService.getAlbums().subscribe(response => {
+      this.albums = response;
+    }, err => console.log(err));
+  }
 
   ngOnInit(): void {
+    this.getAlbums();
   }
 
 }
---
+++ src/app/services/album/album.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+const herokuUrl = 'https://next-groove-api.herokuapp.com';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlbumService {
+
+  constructor(private http: HttpClient) { }
+  getAlbums(): any {
+    return this.http.get(`${herokuUrl}/albums/browse/`);
+}
+}
