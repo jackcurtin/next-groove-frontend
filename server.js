c9d2721141a849d03b3790935270c1a25cd11aaa
Jack Curtin <jackcurtin94@gmail.com>
2021-05-20 17:20:01 -0500

    trying to deploy

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 //Install express server
 const express = require('express');
-const path = require('next-groove');
+const path = require('path');
 
 const app = express();
 
 // Serve only the static files from the dist directory
-// Replace angular-recipes-frontend with your project name
-app.use(express.static(__dirname + '/dist/angular-recipes-frontend'));
+app.use(express.static(__dirname + '/dist/next-groove'));
 
 app.get('/*', function(req,res) {
 
-  // Replace angular-recipes-frontend with your project name
-  res.sendFile(path.join(__dirname+'/dist/angular-recipes-frontend/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/next-groove/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
